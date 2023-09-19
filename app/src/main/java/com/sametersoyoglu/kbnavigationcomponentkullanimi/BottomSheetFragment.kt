1e74a44b6b6ee4a6402f6fd24d148a3102e6e4c6
smter <smtersoyoglu231@gmail.com>
2023-09-20 01:02:55 +0300

    Güncel

---
+++ app/src/main/java/com/sametersoyoglu/kbnavigationcomponentkullanimi/BottomSheetFragment.kt
@@ -0,0 +1,32 @@
+package com.sametersoyoglu.kbnavigationcomponentkullanimi
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.sametersoyoglu.kbnavigationcomponentkullanimi.databinding.FragmentBottomSheetBinding
+
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+    // BottomSheetFragment yapmak için Fragment() yerine BottomSheetDialogFragment() olmak zorunda yoksa gösterme işlemlerini yapamayız.
+    private lateinit var binding: FragmentBottomSheetBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBottomSheetBinding.inflate(inflater,container,false)
+
+        binding.imageViewPaylas.setOnClickListener {
+            Snackbar.make(it,"Paylaşıldı",Snackbar.LENGTH_SHORT).show()
+        }
+
+        binding.imageViewKopyala.setOnClickListener {
+            Snackbar.make(it,"Kopyalandı",Snackbar.LENGTH_SHORT).show()
+        }
+
+        return binding.root
+    }
+
+
+}
