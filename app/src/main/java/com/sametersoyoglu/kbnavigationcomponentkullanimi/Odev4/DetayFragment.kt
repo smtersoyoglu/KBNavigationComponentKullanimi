33d6b18b3b145e61a6cd449dd2a509012b2a6df7
smter <smtersoyoglu231@gmail.com>
2023-09-20 13:56:14 +0300

    Güncel

--- app/src/main/java/com/sametersoyoglu/kbnavigationcomponentkullanimi/Odev4/DetayFragment.kt
+++ app/src/main/java/com/sametersoyoglu/kbnavigationcomponentkullanimi/Odev4/DetayFragment.kt
@@ -1,4 +1,4 @@
-package com.sametersoyoglu.kbnavigationcomponentkullanimi
+package com.sametersoyoglu.kbnavigationcomponentkullanimi.Odev4
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.snackbar.Snackbar
+import com.sametersoyoglu.kbnavigationcomponentkullanimi.R
 import com.sametersoyoglu.kbnavigationcomponentkullanimi.databinding.FragmentDetayBinding
 
 
@@ -41,6 +43,10 @@ class DetayFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,backPress)
+
+        binding.buttonGitB.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_detayFragment_to_sayfaBFragment)
+        }
 
         return binding.root
     }
